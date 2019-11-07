fc6e215de3664258ea379b6812d496e2bf650898
asanka94 <asanka.jsndr@gmail.com>
2019-11-08 00:20:27 +0530

    first commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::group([
+    'middleware' => 'api',
+    'prefix'     => 'auth',
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::patch('update', 'AuthController@update');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
